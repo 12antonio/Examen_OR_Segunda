759656d34b5690d0b9ef29eaaaa6a35f22d8df97
12antonio <antoniobuendia192002@gmail.com>
2022-06-02 19:14:16 +0200

    clase articulo creada

--- src/examen_ordinaria_segunda/Articulo.java
+++ src/examen_ordinaria_segunda/Articulo.java
@@ -10,5 +10,77 @@ package examen_ordinaria_segunda;
  * @author DAW
  */
 public class Articulo {
+    private int identificador;
+    private String nombre;
+    private double precio_venta_cliente;
+    private double precio_compra_proveedor;
+    private double IVA;
+    private int stock;
+
+    
+    public Articulo(int identificador, String nombre, double precio_venta_cliente, double precio_compra_proveedor, double IVA, int stock) {
+        this.identificador = identificador;
+        this.nombre = nombre;
+        this.precio_venta_cliente = precio_venta_cliente;
+        this.precio_compra_proveedor = precio_compra_proveedor;
+        this.IVA = IVA;
+        this.stock = stock;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio_venta_cliente() {
+        return precio_venta_cliente;
+    }
+
+    public double getPrecio_compra_proveedor() {
+        return precio_compra_proveedor;
+    }
+
+    public double getIVA() {
+        return IVA = 21;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+    
+    
+    public void setPrecio_venta_cliente(double precio_venta_cliente) {
+        this.precio_venta_cliente = precio_venta_cliente;
+    }
+
+    public void setPrecio_compra_proveedor(double precio_compra_proveedor) {
+        this.precio_compra_proveedor = precio_compra_proveedor;
+    }
+
+    public void setIVA(double IVA) {
+        this.IVA = IVA;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    
+    public String toString(){
+        return "\n Identificador: " + identificador
+                + "\n Nombre: " + nombre
+                + "\n Precios de venta: " + precio_venta_cliente
+                + "\n Precios de compra: " + precio_compra_proveedor
+                + "\n IVA: " + IVA
+                + "\n Stock: " + stock;
+    }
+
+    
+
+    
+
+    
     
 }
--- src/examen_ordinaria_segunda/Tienda.java
+++ src/examen_ordinaria_segunda/Tienda.java
@@ -5,17 +5,104 @@
  */
 package examen_ordinaria_segunda;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author DAW
  */
 public class Tienda {
+    ArrayList<Articulo> listaArticulos = new ArrayList();
+    static Scanner leer = new Scanner(System.in);
+
+    public static int menu() {
+
+        System.out.println("1. Mostrar artículos");
+        System.out.println("2. Venta a cliente");
+        System.out.println("3. Compra a proveedor");
+        System.out.println("4. Gestionar artículos");
+        System.out.println("5. Salir");
+
+        int opcion = leer.nextInt();
+
+        return opcion;
+        
+    /**
+     *
+     * @return
+     */
 
     /**
-     * @param args the command line arguments
+     *
+     * @param a1
+     * @return
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
     }
+    public String mostrar(){
+
+        String s = null;
+        for (int i = 0; i < this.listaArticulos.size(); i++) {
+
+            Articulo a = (Articulo) this.listaArticulos.get(i);
+
+            s = a.toString();
+
+        } //fin for
+
+        return s;
+}
+
+public static void main(String[] args) {
+        
+       System.out.println("Creamos artículos");
+       
+       Articulo a1 = new Articulo(2, "pepe", 50, 20, 21, 5); 
+       Articulo a2 = new Articulo(3, "juan", 40, 30, 21, 6);
+       Articulo a3 = new Articulo(4, "paco", 30, 10, 21, 7);
+       Articulo a4 = new Articulo(5, "pedro", 20, 40, 21, 8);
+       
+       ArrayList<Articulo> listaArticulos = new ArrayList();
+       
+      listaArticulos.add(a1);
+      listaArticulos.add(a2);
+      listaArticulos.add(a3);
+      listaArticulos.add(a4);
+       
+        boolean bucle =true;
     
+        while(bucle==true){
+            
+            switch(menu()){
+                case 1:
+                    System.out.println("Artículos:");
+                    a1.toString();
+                    a2.toString();
+                    a3.toString();
+                    a4.toString();
+                    
+                    break;
+                    
+                case 2:
+                    System.out.println("El perimetro del pentagono regular es ");
+                    break;
+                    
+                case 3:
+                    
+                    break;
+                    
+                case 4:
+                    bucle=false;
+                    break;
+                    
+                case 5:
+                    bucle=false;
+                    break;
+                    
+                default:
+                    System.out.println("Error, introduce una opcion valida");
+                    break;
+            }  
+        }
+    }
 }
